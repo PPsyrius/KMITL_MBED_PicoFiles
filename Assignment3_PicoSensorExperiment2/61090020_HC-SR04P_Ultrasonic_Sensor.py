a02c01c64a6695ce6d9a6f8b18cc1466be2912bf
PPsyrius <PPsyrius@users.noreply.github.com>
2021-02-25 00:39:59 +0700

    Improve existing code for Assignment3's ultrasonic sensor

--- Assignment3_PicoSensorExperiment2/61090020_HC-SR04P_Ultrasonic_Sensor.py
+++ Assignment3_PicoSensorExperiment2/61090020_HC-SR04P_Ultrasonic_Sensor.py
@@ -1,14 +1,16 @@
 # Assignment 3.2: HC-SR04P Ultrasonic Sensor for Pi Pico
 # Docu: https://www.thaieasyelec.com/product/581/hc-sr04p-ultrasonic-sensor
 
-import machine
-import time
+import utime
 from hcsr04 import HCSR04
 
-sensor = HCSR04(trigger_pin=0, echo_pin=1)
+sensor = HCSR04(trigger_pin=0, echo_pin=1, air_temp=25)
 
 while True:
-    distance_cm = sensor.distance_cm()
-    distance_mm = sensor.distance_mm()
-    print('Distance (cm/mm):', distance_cm, 'cm', ';\t', distance_mm, 'mm')
-    time.sleep(2)
+    try:
+        distance_cm = sensor.distance_cm()
+        distance_mm = sensor.distance_mm()
+        print('Distance (cm/mm):', distance_cm, 'cm;\t', distance_mm, 'mm')
+    except Exception as ex:
+        print('ERROR getting distance:', ex)
+    utime.sleep(2)
